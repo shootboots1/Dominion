65bc05f3b7d3112e29bf6ad5f4657d92f464f843
shootboots1 <144690@newington.nsw.edu.au>
2022-09-18 17:30:57 +1000

    First commit on laptop
    Cleaned up cell consumption script a little bit and updated version from
    2021.3.6f1 to
    2021.3.9f1

--- Dominion/Assets/Scripts/CellConsumption.cs
+++ Dominion/Assets/Scripts/CellConsumption.cs
@@ -31,4 +31,19 @@ public class CellConsumption : MonoBehaviour
             }
         }
     }
+
+    void OnCollisionStay2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("GreenCells"))
+        {
+            if (transform.localScale.x > collision.transform.localScale.x)
+            {
+                transform.localScale -= new Vector3(collision.transform.localScale.x, collision.transform.localScale.y, collision.transform.localScale.z);
+                Destroy(collision.gameObject);
+            }
+            else if (transform.localScale.x < collision.transform.localScale.x){
+                collision.transform.localScale -= new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
